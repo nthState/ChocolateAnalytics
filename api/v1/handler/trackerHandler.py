4d42edda238ad658f4f002b12c77e02c6d2cd3c0
Chris Davis <chrisdavis@Chriss-MacBook-Pro.local>
2014-04-26 14:02:26 +0100

    content-length bug is messing everything up

--- api/v1/handler/trackerHandler.py
+++ api/v1/handler/trackerHandler.py
@@ -10,5 +10,7 @@ class TrackerClassPlural(BaseHandler):
 		self.JSONResponse(response)
 	def post(self, locale):
 		locale = self.GetLocale(locale)
-		response = TrackerService.Track(locale, json.loads(self.request.body))
+		logging.info(self.request.headers['Json'])
+		#response = TrackerService.Track(locale, json.loads(self.request.body))
+		response = TrackerService.Track(locale, json.loads(self.request.headers['Json']))
 		self.JSONResponse(response)
